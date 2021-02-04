b4513527c1d68bacc30e0624fb262e9bd800ea99
Tony <tr33947@gmail.com>
2021-02-04 13:09:36 +0300

    made changes

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     SECRET_KEY = 'tony'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Avamara34@localhost/photographyweb'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     cloudinary.config(
         cloud_name='tee47-galleria',
         api_key='238974632867683',
@@ -23,8 +24,8 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME ='tr33947@gmail.com'
+    MAIL_PASSWORD ='Avamara47'
 
 
     #simple mde configurations
@@ -39,7 +40,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Avamara34@localhost/photographyweb'
     DEBUG = False
 class TestConfig(Config):
     pass
@@ -51,7 +52,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Avamara34@localhost/photographyweb'
     DEBUG = True
 
 config_options = {
---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='tony'
+export DATABASE_URL='postgresql+psycopg2://moringa:Avamara34@localhost/photographyweb'
+export MAIL_USERNAME='tr33947@gmail.com'
+export MAIL_PASSWORD='Avamara47' 
+
+
+python3.8 manage.py server
